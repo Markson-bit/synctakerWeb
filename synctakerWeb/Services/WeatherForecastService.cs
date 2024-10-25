18bc20176b2c92d2da48aaa891add952b11117b3
Marek Małolepszy <marek4924@gmail.com>
2024-10-25 11:42:50 +0200

    fix

---
+++ synctakerWeb/Services/WeatherForecastService.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using synctakerWeb.Models;
+
+public class WeatherForecastService
+{
+    private readonly HttpClient _httpClient;
+
+    public WeatherForecastService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClient = httpClientFactory.CreateClient("WeatherApi");
+    }
+
+    public async Task<IEnumerable<WeatherForecast>> GetWeatherForecastsAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<IEnumerable<WeatherForecast>>("WeatherForecast");
+    }
+}
