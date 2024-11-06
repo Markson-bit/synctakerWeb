e11e5b9e6494241affc66f30559218fa2a7efeff
Marek Małolepszy <marek4924@gmail.com>
2024-11-06 19:19:21 +0100

    password hashing

--- synctakerWeb/Services/UserService.cs
+++ synctakerWeb/Services/UserService.cs
@@ -2,6 +2,8 @@
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using synctakerWeb.Models;
+using System.Security.Cryptography;
+using System.Text;
 
 public class UserService
 {
@@ -12,8 +14,24 @@ public class UserService
         _httpClient = httpClientFactory.CreateClient("ApiClient");
     }
 
-    public async Task<User> GetUserAsync()
+    public async Task<User> AuthenticateUserAsync(string username, string hashPassword)
     {
         return await _httpClient.GetFromJsonAsync<User>("User");
     }
+
+    public string HashPassword(string password)
+    {
+        using (var sha256 = SHA256.Create())
+        {
+            var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+            var builder = new StringBuilder();
+            foreach (var b in bytes)
+            {
+                builder.Append(b.ToString("x2"));
+            }
+            return builder.ToString();
+        }
+    }
+
+    public User CurrentUser { get; set; } // Stores logged User
 }
